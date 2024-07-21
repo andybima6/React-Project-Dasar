94aa15c30c1adc7a135ab45a71b26a84e5ba9fd4
Andy Nugraha Putra <andybima6@gmail.com>
2024-07-22 01:34:52 +0700

    Menambahkan searching pada list

--- src/components/PokemonList/PokemonList.jsx
+++ src/components/PokemonList/PokemonList.jsx
@@ -5,14 +5,34 @@ import "./PokemonList.css";
 
 function PokemonList() {
   const [pokemons] = useState(pokemonJSON);
+  const [filterPokemons ,setFilterPokemons] = useState(pokemonJSON);
 
+  const handleSearch = (event) =>
+  {
+    let search = pokemons.filter((item) => {
+      return item.name.toLowerCase().includes(event.target.value);
+    });
+    setFilterPokemons(search)
+  };
   return (
+    <div>
+    <input type="text" placeholder='Cari Pokemon' className='search' onChange={handleSearch}/>
     <div className="list-pokemon">
-      {pokemons.map((item) => (
-        <PokemonItem key={item.id} pokemon={item} />
-      ))}
-    </div>
-  );
-}
+    {filterPokemons.length == 0 ? (
+      <div>data tidak ditemukan</div>
+    ) : (
+      filterPokemons.map((item) => (
+        <PokemonItem 
+          key={item.id} 
+          pokemon={item} 
+        />
+      ))
+    )}
+  </div>
+  </div>
+  );}
+
+
 
 export default PokemonList;
+
